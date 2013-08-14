09e6fc2cf064c698896105249df6a35a0a086d42
wzp121500 <bb121500@126.com>
2013-08-14 15:21:37 +0800

    Initial Commit

---
+++ TestWzp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TestWzp
+//
+//  Created by wzp on 13-8-14.
+//  Copyright (c) 2013年 TestWzp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
